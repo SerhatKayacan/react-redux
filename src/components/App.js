582b7c1da68f5e1020b12b0ad9a5008e88266ac9
Serhat Kayacan <serhatkayacan006@gmail.com>
2019-05-20 17:51:05 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../App.css";
 import moment from "moment";
 import { connect } from "react-redux";
 import { addReminder, deleteReminder } from "../actions";
